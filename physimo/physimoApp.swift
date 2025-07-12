a036f571a3b1d7d30dc59a1c01e83ac45eeb59bc
Darcy Luo <darcyluojt@gmail.com>
2025-07-12 18:40:57 +0100

    Initial Commit

---
+++ physimo/physimoApp.swift
@@ -0,0 +1,17 @@
+//
+//  physimoApp.swift
+//  physimo
+//
+//  Created by Darcy LUO on 12/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct physimoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ physimoTests/physimoTests.swift
@@ -0,0 +1,17 @@
+//
+//  physimoTests.swift
+//  physimoTests
+//
+//  Created by Darcy LUO on 12/07/2025.
+//
+
+import Testing
+@testable import physimo
+
+struct physimoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
